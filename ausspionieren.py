8f37940f73b09c8c1df1b9a28c69a92ddccbe0b2
cadull <cadull@hotmail.de>
2018-07-28 20:44:19 +0200

    now actually showing position

--- ausspionieren.py
+++ ausspionieren.py
@@ -1,8 +1,13 @@
+import mcpi.minecraft as minecraft
 from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/') # Wir nehmen nun doch mal root, dann muss man weniger tippen im Browser...
 def ausspionieren():
-    positionstext = "Hier soll dann irgendwann mal die Position des Spielers stehen. Da musst Du nun noch passenden Code für schreiben!"
+    mc = minecraft.Minecraft.create()
+    playerIds = mc.getPlayerEntityIds()
+    playerId = playerIds[0]
+    position = mc.entity.getTilePos(playerId)
+    positionstext = "Spielerposition: x=" + str(position.x) + " y=" + str(position.y) + " z=" + str(position.z)
     return render_template('index.html', positionstext=positionstext)
