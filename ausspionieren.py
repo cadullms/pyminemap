edf4379bed91fcac080062bb47b02590992d953b
cadull <cadull@hotmail.de>
2018-07-28 21:09:01 +0200

    exception handling

--- ausspionieren.py
+++ ausspionieren.py
@@ -3,11 +3,14 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/') # Wir nehmen nun doch mal root, dann muss man weniger tippen im Browser...
+@app.route('/')
 def ausspionieren():
-    mc = minecraft.Minecraft.create()
-    playerIds = mc.getPlayerEntityIds()
-    playerId = playerIds[0]
-    position = mc.entity.getTilePos(playerId)
-    positionstext = "Spielerposition: x=" + str(position.x) + " y=" + str(position.y) + " z=" + str(position.z)
-    return render_template('index.html', positionstext=positionstext)
+    try:
+        mc = minecraft.Minecraft.create()
+        playerIds = mc.getPlayerEntityIds()
+        playerId = playerIds[0]
+        position = mc.entity.getTilePos(playerId)
+        positionstext = "Spielerposition: x=" + str(position.x) + " y=" + str(position.y) + " z=" + str(position.z)
+        return render_template('index.html', positionstext=positionstext)
+    except (ConnectionResetError, ConnectionRefusedError):
+        return render_template('index.html', positionstext="No minecraft instance available.")
